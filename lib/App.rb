08f84279fb2c7e8f35146157fbd39f7e9d4739da
Wyatt <wyatt.burkhardt@gmail.com>
2020-04-17 16:10:04 -0400

    refactor App into a class to better handle routing.  add root and 404

--- lib/App.rb
+++ lib/App.rb
@@ -1,5 +1,16 @@
 class App
-  def call(env)
-    [200, { 'Content-Type' => 'text/plain' }, ['Hello, World!']]
+  def self.call(env)
+    new(env).response
+  end
+
+  def initialize(env)
+    @request = Rack::Request.new(env)
+  end
+
+  def response
+    case @request.path
+    when '/' then [200, { 'Content-Type' => 'text/plain' }, ['Hello, World!']]
+    else [404, { 'Content-Type' => 'text/plain' }, ['Not Found']]
+    end
   end
 end
