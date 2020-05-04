9ff164267e027d63262138c143bea83a1939d946
Wyatt <wyatt.burkhardt@gmail.com>
2020-05-04 10:15:38 -0400

    Update router to handle different methods - update the router to manually handle the post/get routing

--- lib/post.rb
+++ lib/post.rb
@@ -2,4 +2,8 @@ class Post
   def index
     [200, { 'Content-Type' => 'text/plain' }, ['Hello from Post#index']]
   end
+
+  def create
+    [200, { 'Content-Type' => 'text/plain' }, ['Hello from Post#create']]
+  end
 end
